cdea5ac5afe6c3b0bb9066aa2857005a40b307c4
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-04-09 16:38:13 +0300

    add migrations

---
+++ migrations/000001_add_main_tables.down.sql
@@ -0,0 +1,13 @@
+begin;
+drop table user_roles cascade;
+
+drop table role cascade;
+
+drop table block_list cascade;
+
+drop table logs cascade;
+
+drop table conversation cascade;
+
+drop table users cascade;
+COMMIT;
