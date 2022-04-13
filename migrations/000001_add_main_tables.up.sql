64d3dd2df5f8191ebdb68a07619c54dd911d9e11
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-04-13 23:54:24 +0300

    fix postgresql script according to holistic

--- migrations/000001_add_main_tables.up.sql
+++ migrations/000001_add_main_tables.up.sql
@@ -1,15 +1,6 @@
 BEGIN;
--- Table role
-create table role
-(
-    id   serial
-        constraint role_pk
-            primary key,
-    name VARCHAR(20) not null
-);
 
-create unique index role_name_uindex
-    on role (name);
+CREATE TYPE role AS enum ('admin', 'banned');
 
 -- Table users
 create table users
@@ -31,23 +22,19 @@ create unique index user_chat_id_uindex
 -- Table user_roles
 create table user_roles
 (
-    user_id int not null
+    user_id       int
         constraint user_roles_users_id_fk
-            references users,
-    role_id int not null
-        constraint user_roles_role_id_fk
-            references role,
-    constraint user_roles_pk
-        primary key (user_id, role_id)
+            references users primary key,
+    assigned_role role
 );
 
 -- Table blocked
 create table block_list
 (
-    list_author     int not null
+    list_author     int
         constraint block_list_users_id_id_fk
             references users,
-    blocked_user_id int not null
+    blocked_user_id int
         constraint block_list_users_blocked_user_id_id_fk
             references users,
     constraint block_list_pk
@@ -69,19 +56,26 @@ create table conversation
             references users
 );
 
+CREATE INDEX ON conversation USING hash (sender);
+CREATE INDEX ON conversation USING hash (receiver);
+
 -- Table logs
 create table logs
 (
-    id              serial
+    id                serial
         constraint logs_pk
             primary key,
-    message         VARCHAR(1000)           not null,
-    date            timestamp default now() not null,
-    sender          int                     not null
+    message           VARCHAR(1000)           not null,
+    message_timestamp timestamp default now() not null,
+    sender            int                     not null
         constraint logs_users_id_fk
             references users,
-    conversation_id int                     not null
+    conversation_id   int                     not null
         constraint logs_conversations_id references conversation
 );
+
+CREATE INDEX ON logs USING hash (sender);
+CREATE INDEX ON logs USING hash (conversation_id);
+
 
 COMMIT;
