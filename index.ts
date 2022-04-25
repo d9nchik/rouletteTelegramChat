80b7ddd28c6a3e0080ff12dd13f6e01cf8723f38
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-04-25 19:40:52 +0300

    add my_identity command

--- index.ts
+++ index.ts
@@ -1,21 +1,33 @@
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 import { Chat } from 'telegraf/typings/core/types/typegram';
-import { generateAge, generateName, getUserID } from './src/core/user';
+import {
+  generateAge,
+  generateName,
+  getUserID,
+  getUserStatus,
+} from './src/core/user';
 import { CreateUser } from './src/types/user';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || '');
 
-bot.start(async ctx => {
-  const chat = ctx.chat;
-  if (chat.type !== 'private' || !chat.username) {
-    return ctx.reply(`Sorry, I can only work in private chats.`);
-  }
+bot.start(async ctx =>
+  ctx.reply(
+    await insureChatIsPrivate(ctx.chat, async chat => {
+      await getUserID(getCreateUser(chat));
+      return `Hi👋!`;
+    })
+  )
+);
 
-  await getUserID(getCreateUser(chat));
+bot.command('my_identity', async ctx =>
+  ctx.reply(
+    await insureChatIsPrivate(ctx.chat, chat =>
+      getUserStatus(getCreateUser(chat))
+    )
+  )
+);
 
-  return ctx.reply(`Hi👋!`);
-});
 bot.help(ctx => ctx.reply('Send me a sticker'));
 bot.on('sticker', ctx => ctx.reply('👍'));
 bot.hears('hi', ctx => ctx.reply('Hey there'));
@@ -24,6 +36,17 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+async function insureChatIsPrivate(
+  chat: Chat.PrivateChat | Chat.GroupChat | Chat.SupergroupChat,
+  fn: (chat: Chat.PrivateChat) => Promise<string>
+): Promise<string> {
+  if (chat.type !== 'private') {
+    return `Sorry, I can only work in private chats.`;
+  }
+
+  return fn(chat);
+}
 
 function getCreateUser(chat: Chat.PrivateChat): CreateUser {
   return {
