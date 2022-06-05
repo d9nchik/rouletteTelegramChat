f12c791905adcfb16822dfb88eea90a76c9fe1bb
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-06-05 18:57:56 +0300

    add flag if exists to to table down

--- migrations/000001_add_main_tables.down.sql
+++ migrations/000001_add_main_tables.down.sql
@@ -1,13 +1,8 @@
-begin;
-drop table user_roles cascade;
-
-DROP TYPE role;
-
-drop table block_list cascade;
-
-drop table logs cascade;
-
-drop table conversation cascade;
-
-drop table users cascade;
+BEGIN;
+DROP TABLE IF EXISTS user_roles cascade;
+DROP TYPE IF EXISTS role;
+DROP TABLE IF EXISTS block_list cascade;
+DROP TABLE IF EXISTS logs cascade;
+DROP TABLE IF EXISTS conversation cascade;
+DROP TABLE IF EXISTS users cascade;
 COMMIT;
